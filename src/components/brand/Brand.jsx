d299f4ddabbb51797bdb0ff029a64af3f9dfada8
egwimcodes <egwimcodes@gmail.com>
2024-01-04 11:35:35 +0100

    worked on header and brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './import'
 const Brand = () => {
   return (
-    <div>
+    <div className='gpt4__brand section__padding'>
+      <div className="">
+        <img src={google} alt="google" srcset="" />
+      </div>
+      <div className="">
+        <img src={slack} alt="slack" srcset="" />
+      </div>
+      <div className="">
+        <img src={atlassian} alt="atlassian" srcset="" />
+      </div>
+      <div className="">
+        <img src={dropbox} alt="dropbox" srcset="" />
+      </div>
+      <div className="">
+        <img src={shopify} alt="shopify" srcset="" />
+      </div>
       
     </div>
   )
