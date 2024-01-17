8744f972bfbe761368aa1a418e5407d8d153254e
egwimcodes <egwimcodes@gmail.com>
2024-01-17 21:54:13 +0100

    modified images

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import people from '../../assets/people.png'
-import ai from '../../assets/ai.jpg'
+import ai from '../../assets/ai.png'
 const Header = () => {
   return (
     <div className='gpt4__header section__padding' id='home'>
